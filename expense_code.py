fa37400de223e1ed72a0074d27495be78123b9ee
sabdulmajid <ayman.hasib@outlook.com>
2023-05-29 23:08:52 +0300

    changed code to output directly to excel file

--- expense_code.py
+++ expense_code.py
@@ -22,6 +22,18 @@ sent_transfers = filtered_expenses[filtered_expenses['Transaction Type'].str.sta
 # Sum the sent e-transfers for the specific month
 total_sent_transfers = sent_transfers['Expense'].sum()
 
-# Output the results
+# Create a DataFrame with the totals
+totals_df = pd.DataFrame({
+    'Transaction Type': ['Total expenses', 'Total sent e-transfers (without rent)'],
+    'Expense': [total_expenses, total_sent_transfers-1000]
+})
+
+# Append the totals DataFrame to the original DataFrame
+df = pd.concat([df, totals_df], ignore_index=True)
+
+# Write the updated DataFrame back to the Excel file
+df.to_excel('expenses.xlsx', index=False)
+
+# Output the results with 2 decimal places
 print(f"Total expenses for {target_month}/{target_year}: ${total_expenses:.2f}")
-print(f"Total sent e-transfers for {target_month}/{target_year}: ${total_sent_transfers-1000:.2f}")
+print(f"Total sent e-transfers (without rent) for {target_month}/{target_year}: ${total_sent_transfers-1000:.2f}")
