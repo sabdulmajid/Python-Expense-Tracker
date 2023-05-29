8001cb75c1f1ecc3478130eaff6e5b0c543a820a
sabdulmajid <ayman.hasib@outlook.com>
2023-05-29 23:07:27 +0300

    pandas code in python + dummy expenses

---
+++ expense_code.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Read the Excel file
+df = pd.read_excel('expenses.xlsx')
+
+# Convert the date column to a datetime data type
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+
+# Define the month and year you're interested in
+target_month = 1
+target_year = 2023
+
+# Filter the expenses for the specific month and year
+filtered_expenses = df[(df['Date'].dt.month == target_month) & (df['Date'].dt.year == target_year)]
+
+# Sum the expenses for the specific month
+total_expenses = filtered_expenses['Expense'].sum()
+
+# Find all sent e-transfers in the specific month
+sent_transfers = filtered_expenses[filtered_expenses['Transaction Type'].str.startswith('SEND E-TFR')]
+
+# Sum the sent e-transfers for the specific month
+total_sent_transfers = sent_transfers['Expense'].sum()
+
+# Output the results
+print(f"Total expenses for {target_month}/{target_year}: ${total_expenses:.2f}")
+print(f"Total sent e-transfers for {target_month}/{target_year}: ${total_sent_transfers-1000:.2f}")
